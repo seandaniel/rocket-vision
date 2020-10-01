4c977c5fec47095b9fe366c9c10db283e50f605c
Sean de Groot <degroot.sd@gmail.com>
2020-09-30 20:19:53 -0400

    Added wp_title()

--- footer.php
+++ footer.php
@@ -3,7 +3,15 @@
 <footer>
   <div class="wrapper">
     <div class="title-nav-container">
-      <h3><?php wp_title() ?></h3>
+      <h3><?php
+
+      if ($title !== 'Rocket Vision') {
+        $title = 'Rocket Vision';
+      } else {
+        $title = wp_title();
+      }
+
+      echo $title; ?></h3>
       <?php wp_nav_menu( array(
         'theme_location' => 'social',
         'container_class' => 'social-menu'
