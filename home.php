acc2064511f10c2ccb731e81dbd2ba0bf13136f9
Sean de Groot <degroot.sd@gmail.com>
2020-10-02 17:11:55 -0400

    Finished homepage

--- home.php
+++ home.php
@@ -5,15 +5,24 @@
 <div class="wrapper">
 
   <section class="hero-container">
-    <h1> <?php the_field('hero_heading'); ?>
-    <p> <?php the_field('hero_text_paragraph'); ?>
+    <h1> <?php the_field('hero_heading'); ?></h1>
+    <p> <?php the_field('hero_text_paragraph'); ?></p>
     <?php
 
       $link = get_field('hero_link');
 
       if( $link ) { ?>
-        <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
+        <a class="button" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
       <?php } 
+
+    ?>
+    <?php 
+    
+      $image = get_field('hero_image');
+      
+      if( $image ) {
+        echo wp_get_attachment_image( $image, 'large');
+      } 
 
     ?>
   </section>
@@ -28,7 +37,7 @@
         $link = get_field('about_link');
 
         if( $link ) { ?>
-          <a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
+          <a class="button" href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>"><?php echo $link['title']; ?></a>
         <?php } 
       
       ?>
@@ -38,10 +47,28 @@
       $image = get_field('about_image');
       
       if( $image ) {
-        echo wp_get_attachment_image( $image, 'thumbnail');
+        echo wp_get_attachment_image( $image, 'large');
       } 
 
     ?>
+  </section>
+
+  <section class="employee-text-img-container">
+    <?php 
+    
+      $image = get_field('employee_image');
+        
+      if( $image ) {
+        echo wp_get_attachment_image( $image, 'large');
+      } 
+
+    ?>
+    <div class="employee-text">
+      <h3><?php the_field('job_title'); ?></h3>
+      <h2><?php the_field('employee_name'); ?></h2>
+      <p><?php the_field('employee_paragraph_1'); ?></p>
+      <p><?php the_field('employee_paragraph_2'); ?></p>
+    </div>
   </section>
 
 </div> 
