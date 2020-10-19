aeddce6de5e7fffc9ebb643201d827391a2f0ae2
Sean de Groot <degroot.sd@gmail.com>
2020-10-19 16:13:18 -0400

    Category things

--- category.php
+++ category.php
@@ -3,7 +3,7 @@
 <div class="wrapper">
   <div class="content">
     <div class="secondary-heading-container">
-      <h1 class="category-title"><?php single_cat_title(); ?></h1>
+      <h1><?php single_cat_title(); ?></h1>
       <?php echo get_the_post_thumbnail(get_option( 'page_for_posts' ), 'full'); ?>
       </div>
       <?php
